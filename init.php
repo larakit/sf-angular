8fae36e452045c981e8ce2a2f14a5eead03941a5
Alexey Berdnikov <aberdnikov@gmail.com>
2016-04-25 12:27:52 +0500

    1.4.10

--- init.php
+++ init.php
@@ -1,3 +1,3 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular')
-                            ->js('//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.20/angular.min.js');
+                            ->js('//cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.10/angular.min.js');
