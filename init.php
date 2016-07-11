0bed2d63874dfae47ef023a452fbf6237d3debb3
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-11 21:41:33 +0500

    fixed

--- init.php
+++ init.php
@@ -1,3 +1,4 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular')
-                            ->js('//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.5/angular.js');
+    ->setSourceDir('public')
+    ->jsPackage('angular.min.js');
